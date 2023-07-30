3a65893244394c75ee0bb7242326f20d68cc79a6
KenCorp <kennethwarui35@gmail.com>
2023-07-30 09:11:13 +0300

    Added css

--- src/BotCollection.js
+++ src/BotCollection.js
@@ -1,26 +1,28 @@
 import { useState, useEffect } from "react";
 import React from "react";
+import "./BotCollection.css"; 
 
-function BotCollection(){
-    const [profile, setProfile] = useState([]);
+function BotCollection() {
+  const [profile, setProfile] = useState([]);
 
-    useEffect(() => {
-        fetch('http://localhost:3000/bots')
-        .then(resp => resp.json())
-        .then(data => setProfile(data))
-    }, [])
+  useEffect(() => {
+    fetch('http://localhost:3000/bots')
+      .then(resp => resp.json())
+      .then(data => setProfile(data));
+  }, []);
 
-    return (
-        <div>
-            <h1>Bot Collection</h1>
-            {profile.map((bot, index)=> (
-                <span key={index}>
-                    <img src={bot.avatar_url} alt=""/>
-                    <h2>{bot.name}</h2>
-                    <p>{bot.catchphrase}</p>
-                </span>
-            ))}
+  return (
+    <div className="bot-container">
+      <h1>Bot Collection</h1>
+      {profile.map((bot, index) => (
+        <div key={index} className="bot-card">
+          <img className="bot-avatar" src={bot.avatar_url} alt="" />
+          <h2 className="bot-name">{bot.name}</h2>
+          <p className="bot-catchphrase">{bot.catchphrase}</p>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
+
 export default BotCollection;
