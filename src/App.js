dbfb26db6755a4c7f88747f0cff1f8e6cf19127a
KenCorp <kennethwarui35@gmail.com>
2023-07-30 15:44:59 +0300

    Added delete feature

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import BotCollection from './BotCollection';
 import YourBotArmy from './YourBotArmy';
+import { useState } from 'react';
 
 function App() {
+  const [enlistedBot, setEnlistedBots] = useState([]);
+
+  function deleteBot(botId) {
+    setEnlistedBots(enlistedBot.filter((bot) => bot.id !== botId));
+  }
+
   return (
     <Routes>
-      <Route path="/" element={<BotCollection />} />
-      <Route path="/yourbotarmy" element={<YourBotArmy />} />
+      <Route path="/" element={<BotCollection enlistedBots={enlistedBot} />} />
+      <Route path="/yourbotarmy" element={<YourBotArmy onDelete={deleteBot} enlistedBots={enlistedBot} />} />
     </Routes>
   );
 }
--- src/YourBotArmy.js
+++ src/YourBotArmy.js
@@ -1,12 +1,23 @@
 import React from "react";
 
-const YourBotArmy = ({ enlistedBots }) => {
+const YourBotArmy = ({ enlistedBots, onDelete }) => {
   const bots = enlistedBots ?? [];
-    return (
+
+  function handleDelete(botId) {
+    fetch(`http://localhost:3000/bots/${botId}`, {
+      method: "DELETE",
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        onDelete(data);
+      })
+  }
+
+  return (
     <div>
       <h1>Your Bot Army</h1>
       {bots.map((bot) => (
-        <div key={bot.id}>
+        <div key={bot.id} onClick={() => handleDelete(bot.id)}>
           <img className="bot-avatar" src={bot.avatar_url} alt="" />
           <h2 className="bot-name">{bot.name}</h2>
           <p className="bot-catchphrase">{bot.catchphrase}</p>
